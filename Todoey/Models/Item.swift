b770b9763cfbdb5d9a5a0833845a3d90b6c48490
vali <vali.ciuca@gmail.com>
2018-06-03 01:44:38 +0300

    Refactoring as MVC

---
+++ Todoey/Models/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Valeriu Ciuca on 6/3/18.
+//  Copyright © 2018 Valeriu Ciuca. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
